30c422fb905456226149cf6c325f992468e845c6
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2025-12-05 10:59:33 +0000

    add doom config

---
+++ config/doom/custom.el
@@ -0,0 +1,62 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
+ '(custom-safe-themes
+   '("d97ac0baa0b67be4f7523795621ea5096939a47e8b46378f79e78846e0e4ad3d"
+     "0f1341c0096825b1e5d8f2ed90996025a0d013a0978677956a9e61408fcd2c77"
+     "c5878086e65614424a84ad5c758b07e9edcf4c513e08a1c5b1533f313d1b17f1"
+     "9013233028d9798f901e5e8efb31841c24c12444d3b6e92580080505d56fd392"
+     "ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c"
+     "7964b513f8a2bb14803e717e0ac0123f100fb92160dcf4a467f530868ebaae3e"
+     "51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138"
+     "636b135e4b7c86ac41375da39ade929e2bd6439de8901f53f88fde7dd5ac3561"
+     "7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52"
+     "a44e2d1636a0114c5e407a748841f6723ed442dc3a0ed086542dc71b92a87aee"
+     "991ca4dbb23cab4f45c1463c187ac80de9e6a718edc8640003892a2523cb6259"
+     "683b3fe1689da78a4e64d3ddfce90f2c19eb2d8ab1bab1738a63d8263119c3f4"
+     "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644"
+     "dc8285f7f4d86c0aebf1ea4b448842a6868553eded6f71d1de52f3dcbc960039"
+     "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851"
+     "1cae4424345f7fe5225724301ef1a793e610ae5a4e23c023076dc334a9eb940a"
+     "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525"
+     "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
+     "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00"
+     "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" default))
+ '(exwm-floating-border-color "#191b20")
+ '(fci-rule-color "#5B6268")
+ '(highlight-tail-colors
+   ((("#333a38" "#99bb66" "green") . 0) (("#2b3d48" "#46D9FF" "brightcyan") . 20)))
+ '(ignored-local-variable-values
+   '((org-reveal-title-slide
+      . "<h1>%t</h1><br><h4>%a<br>%d<br>University of Glasgow</h4><img src='images/LSHTM_Logo_Black.png' height=130> <img src='images/logo.png'>")
+     (TeX-engine . xetex)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(objed-cursor-color "#ff6c6b")
+ '(pdf-view-midnight-colors (cons "#bbc2cf" "#282c34"))
+ '(rustic-ansi-faces
+   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
+ '(safe-local-variable-values
+   '((org-reveal-title-slide
+      . "<h1>%t</h1><br><h4>%a<br>%d<br>recon gathering, London</h4><img src='images/LSHTM_Logo_Black.png' height=130> <img src='images/logo.png'>")))
+ '(vc-annotate-background "#282c34")
+ '(vc-annotate-color-map
+   (list (cons 20 "#98be65") (cons 40 "#b4be6c") (cons 60 "#d0be73")
+         (cons 80 "#ECBE7B") (cons 100 "#e6ab6a") (cons 120 "#e09859")
+         (cons 140 "#da8548") (cons 160 "#d38079") (cons 180 "#cc7cab")
+         (cons 200 "#c678dd") (cons 220 "#d974b7") (cons 240 "#ec7091")
+         (cons 260 "#ff6c6b") (cons 280 "#cf6162") (cons 300 "#9f585a")
+         (cons 320 "#6f4e52") (cons 340 "#5B6268") (cons 360 "#5B6268")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
