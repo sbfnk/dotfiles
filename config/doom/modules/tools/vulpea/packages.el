0d8bb5c753b3a9aa21379eef0a93dc886e32b655
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2026-02-03 09:49:08 +0000

    Disable consult-vulpea due to new note creation bug
    consult-vulpea causes empty title/slug when creating new notes
    in contexts with few existing notes. Standard vulpea-find works.
    
    Co-authored-by: sbfnk-bot <242615673+sbfnk-bot@users.noreply.github.com>

--- config/doom/modules/tools/vulpea/packages.el
+++ config/doom/modules/tools/vulpea/packages.el
@@ -4,6 +4,6 @@
 ;; vulpea v2 - standalone, no org-roam dependency
 (package! vulpea)
 
-;; Optional: enhanced selection with consult
-(package! consult-vulpea
-  :recipe (:host github :repo "d12frosted/consult-vulpea"))
+;; Disabled: causes empty title/slug when creating new notes
+;; (package! consult-vulpea
+;;   :recipe (:host github :repo "d12frosted/consult-vulpea"))
